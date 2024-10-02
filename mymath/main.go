cd24773cb24b2855120df1a1b007b8b4322cc8ce
Madpeacher <rainbownatural@yandex.ru>
2024-10-02 15:51:09 +0300

    googog

--- mymath/main.go
+++ mymath/main.go
@@ -24,3 +24,15 @@ func Floor(x float64) float64 {
 func Pow(x, y float64) float64 {
 	return math.Pow(x, y)
 }
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
